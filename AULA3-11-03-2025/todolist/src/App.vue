5ee8b992b703430bec1fcb7ea01fe2ffeb3b27b7
ivanlppires <ivanpires@gmail.com>
2025-03-11 19:35:18 -0400

    Atualização do README

---
+++ AULA3-11-03-2025/todolist/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="login" v-if="!isAuthenticated">
+    <h1>Autenticação</h1>
+    <input type="text" placeholder="Usuário">
+    <input type="password" placeholder="Senha">
+    <button>Entrar</button>
+  </div>
+  <div id="todolist" v-else>
+    <h1>Lista de Tarefas</h1>
+    <input type="text" placeholder="Nova tarefa">
+    <button>Adicionar</button>
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+  </div>
+</template>
+<script setup>
+  import { ref } from 'vue';
+  const isAuthenticated = ref(false);
+</script>
+<style>
+#login {
+  max-width: 700px;
+  margin: auto;
+}
+
+input {
+  width: 100%;
+  padding: 10px;
+  margin: 10px 0;
+}
+</style>
