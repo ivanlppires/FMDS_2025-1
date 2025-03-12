ea7c8877c8bd3a9e49fe6c6422080c1a6e2bcb98
ivanlppires <ivanpires@gmail.com>
2025-03-11 21:31:45 -0400

    exemplo de componentes, props e emits, v-if, v-else e v-for, watch e watcheffect

--- AULA3-11-03-2025/todolist/src/components/Login.vue
+++ AULA3-11-03-2025/todolist/src/components/Login.vue
@@ -4,20 +4,52 @@
         <input type="text" placeholder="Usuário" v-model="user">
         <input type="password" placeholder="Senha" v-model="pass">
         <button @click="login">Entrar</button>
+        <p>Verificação da senha</p>
+        <ul>
+            <li v-if="!check.size">A senha deve ter no mínimo 6 caracteres</li>
+            <li v-if="!check.lowercase">A senha deve ter no mínimo 1 letra minúscula</li>
+            <li v-if="!check.uppercase">A senha deve ter no mínimo 1 letra maiúscula</li>
+            <li v-if="!check.number">A senha deve ter no mínimo 1 número</li>
+            <li v-if="!check.special">A senha deve ter no mínimo 1 caractere especial</li>     
+        </ul>
     </div>
 </template>
 
 <script setup>
-import { ref, defineProps, defineEmits } from 'vue';
+import { ref, defineProps, defineEmits, watch, watchEffect, onBeforeMount, onMounted } from 'vue';
 const emits = defineEmits(['authenticated']);
 const props = defineProps({ color: String });
 const user = ref('');
 const pass = ref('');
+const check = ref({
+    size: false,
+    lowercase: false,
+    uppercase: false,
+    number: false,
+    special: false
+});
 const login = () => {
     if (user.value === 'ivan' && pass.value === '123') {
         emits('authenticated', true);
     }
 };
+// observa uma variável específica, ou um conjunto deles.
+watch(pass, (value) => {
+    check.value.size = value.length > 6
+    check.value.lowercase = /[a-z]/.test(value)
+    check.value.uppercase = /[A-Z]/.test(value)
+    check.value.number = /[0-9]/.test(value)
+    check.value.special = /[^a-zA-Z0-9]/.test(value)
+});
+// watch effect é uma função que é executada sempre que uma variável reativa é alterada.
+watchEffect(() => {
+    console.log('Senha:', pass.value);
+    console.log('Usuário:', user.value);
+    console.log('Tamanho:', check.value.size);
+});
+onMounted(() => {
+    window.alert('Componente montado');
+});
 </script>
 <style>
 #login {
